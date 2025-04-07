bcb29adbbf3ff23773469eb9bd9b2f8e575cc7ac
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-07 14:54:59 -0700

    Style hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,23 @@
 export const Hero = () => {
   return (
-    <section id="hero" className="min-h-screen">
-      <h1>Spencer Lee</h1>
-      <h2>Software Engineer</h2>
-      <p>I build stuff.</p>
+    <section
+      id="hero"
+      className="flex min-h-screen flex-col items-center justify-center"
+    >
+      <div>
+        <h1 className={`font-ibm-plex-mono text-accent ml-0.5 text-xl`}>
+          Hi there, my name is
+        </h1>
+        <h2 className="-ml-0.5 mt-3 text-7xl font-bold">Spencer Lee.</h2>
+        <h3 className="text-muted mt-5 text-6xl font-bold">
+          I build digital experiences.
+        </h3>
+        <p className="text-muted ml-0.5 mt-8 max-w-[540px]">
+          I'm a software engineer who specializes in creating accessible,
+          pixel-perfect interfaces. I'm obsessed with craftsmanship, always
+          aiming for that perfect blend of performance, usability, and design.
+        </p>
+      </div>
     </section>
   );
 };
