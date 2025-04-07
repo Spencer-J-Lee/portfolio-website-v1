cb0818e42bc9c8b55b7591fb60acf1b7ed5b5cdd
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-07 12:39:10 -0700

    Style social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -7,26 +7,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const SocialLinks = () => {
   return (
-    <aside className="fixed bottom-0 left-0 ml-7 flex flex-col items-center gap-7">
+    <aside className="fixed bottom-0 left-7 flex flex-col items-center gap-7 lg:left-12">
       <ul className="flex flex-col items-center gap-5">
         <li>
           <a href="https://www.github.com/Spencer-J-Lee" target="_blank">
-            <FontAwesomeIcon icon={faGithub} size="lg" />
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="lg"
+              className="text-primary transition-colors hover:text-highlight"
+            />
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/spenjlee/" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="lg"
+              className="text-primary transition-colors hover:text-highlight"
+            />
           </a>
         </li>
         <li>
           <a href="https://www.codepen.io/MisterJibbs" target="_blank">
-            <FontAwesomeIcon icon={faCodepen} size="lg" />
+            <FontAwesomeIcon
+              icon={faCodepen}
+              size="lg"
+              className="text-primary transition-colors hover:text-highlight"
+            />
           </a>
         </li>
       </ul>
 
-      <div className="h-28 w-min border-l-2 border-accent" />
+      <div className="h-28 w-min border-l-2 border-primary" />
     </aside>
   );
 };
