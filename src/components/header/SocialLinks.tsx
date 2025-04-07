1c479afcb0661c7d8f75d5a7de092c5f3a8feb46
Spencer Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-06 17:03:32 -0700

    Fix social links to the side

--- src/components/header/SocialLinks.tsx
+++ src/components/header/SocialLinks.tsx
@@ -7,24 +7,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const SocialLinks = () => {
   return (
-    <section>
-      <ul>
+    <aside className="fixed bottom-0 left-0 ml-7 flex flex-col items-center gap-7">
+      <ul className="flex flex-col items-center gap-5">
         <li>
           <a href="https://www.github.com/Spencer-J-Lee" target="_blank">
-            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faGithub} size="lg" />
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/spenjlee/" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
           </a>
         </li>
         <li>
           <a href="https://www.codepen.io/MisterJibbs" target="_blank">
-            <FontAwesomeIcon icon={faCodepen} />
+            <FontAwesomeIcon icon={faCodepen} size="lg" />
           </a>
         </li>
       </ul>
-    </section>
+
+      <div className="h-28 w-min border-l-2 border-accent" />
+    </aside>
   );
 };
