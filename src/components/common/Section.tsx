721de90afd07ab07b3197b0f8224388d6ce3828e
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-08 13:50:44 -0700

    Add section dividers

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -12,7 +12,13 @@ export const Section = ({ title, children, id }: SectionProps) => {
     <>
       {id && <ScrollAnchor id={id} />}
       <section className="mx-auto max-w-[900px]">
-        <h4 className="mb-6 text-4xl font-semibold">{title}</h4>
+        <div className="mb-6 flex items-center gap-5">
+          <h4 className="text-4xl font-semibold">{title}</h4>
+          <div className="flex max-w-80 flex-1 flex-col justify-between gap-1">
+            <div className="border-accent/50 w-2/3 border-b border-dashed" />
+            <div className="border-accent/70 w-full border-b" />
+          </div>
+        </div>
         {children}
       </section>
     </>
