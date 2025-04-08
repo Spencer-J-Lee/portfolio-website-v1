3e637426693a5faee8e75d3716de6823088011da
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-08 13:20:29 -0700

    Add different sizes for button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,12 +1,28 @@
-import { ReactNode } from "react";
+import clsx from "clsx";
+import { ReactNode, useMemo } from "react";
 
 interface ButtonProps {
   children: ReactNode;
+  size?: "sm" | "md";
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, size = "md" }: ButtonProps) => {
+  const sizeClassName = useMemo(() => {
+    switch (size) {
+      case "sm":
+        return "px-4 py-2 text-sm";
+      case "md":
+        return "px-10 py-3 text-base";
+    }
+  }, [size]);
+
   return (
-    <button className="border-accent text-accent font-ibm-plex-mono rounded border px-10 py-3 transition-all duration-200 hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_#6da2ff]">
+    <button
+      className={clsx(
+        "border-accent text-accent font-ibm-plex-mono rounded border transition-all duration-200 hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_#6da2ff]",
+        sizeClassName,
+      )}
+    >
       {children}
     </button>
   );
