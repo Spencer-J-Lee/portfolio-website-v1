9c1fca996c1d0327a2b124bdcffda382b4df8654
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-08 11:20:20 -0700

    Style experience section

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,3 +1,4 @@
+import { SECTIONS } from "@/constants/sections";
 import { Pill } from "../common/Pill";
 import { Section } from "../common/Section";
 import { StyledLink } from "../common/StyledLink";
@@ -5,20 +6,27 @@ import { experienceData } from "./data";
 
 export const Experience = () => {
   return (
-    <Section id="experience" title="Experience">
-      <ul>
+    <Section id={SECTIONS.EXPERIENCE} title="Experience">
+      <ul className="flex flex-col justify-between gap-10">
         {experienceData.map(
           ({ title, company, description, dateStr, link, skills }) => (
             <li key={title + company}>
-              <div className="grid grid-cols-[1fr_4fr] gap-10">
-                <div className="">{dateStr}</div>
-                <div className="">
-                  <StyledLink
-                    href={link}
-                    external
-                  >{`${title} - ${company}`}</StyledLink>
-                  <p>{description}</p>
-                  <ul className="flex flex-wrap gap-2.5">
+              <div className="relative grid grid-cols-[1fr_6fr] gap-8">
+                <div className="text-muted/70 mt-0.5 text-right text-sm tracking-wider">
+                  {dateStr}
+                </div>
+
+                <div>
+                  <StyledLink href={link} external>
+                    {`${title} - ${company}`}
+                    <span className="absolute -bottom-4 -left-4 -right-4 -top-3 rounded transition-colors hover:bg-slate-500/10" />
+                  </StyledLink>
+
+                  <p className="text-muted mt-2 text-sm leading-relaxed">
+                    {description}
+                  </p>
+
+                  <ul className="mt-4 flex flex-wrap gap-2.5">
                     {skills.map((skill) => (
                       <li key={skill}>
                         <Pill text={skill} />
