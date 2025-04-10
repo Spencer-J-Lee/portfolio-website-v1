03b6b15b138391e48f78fff169d69872d86ef0d7
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-09 17:08:02 -0700

    Update skip to content button with new component

--- src/components/common/SkipToContent.tsx
+++ src/components/common/SkipToContent.tsx
@@ -1,12 +1,13 @@
 import { SECTIONS } from "@/constants/sections";
+import { AnchorButton } from "./buttons/AnchorButton";
 
 export const SkipToContent = () => {
   return (
-    <a
+    <AnchorButton
       href={`#${SECTIONS.CONTENT}`}
-      className="from-accent/50 to-highlight/80 absolute left-1/2 top-0 z-50 -translate-x-1/2 -translate-y-full rounded-b bg-gradient-to-r px-6 py-2 text-lg font-bold transition-transform focus-visible:translate-y-0"
+      className="absolute left-1/2 top-0 z-50 -translate-x-1/2 -translate-y-full rounded-t-none transition-all focus-visible:translate-y-0"
     >
       Skip to Content
-    </a>
+    </AnchorButton>
   );
 };
