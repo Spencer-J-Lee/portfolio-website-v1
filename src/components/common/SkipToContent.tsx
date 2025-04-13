2abb9417b637c6d03b4e20d6aa2c1fc61b9c3fbe
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-13 15:09:47 -0700

    Fix skip button visible on mobile before focus

--- src/components/common/SkipToContent.tsx
+++ src/components/common/SkipToContent.tsx
@@ -5,7 +5,7 @@ export const SkipToContent = () => {
   return (
     <AnchorButton
       href={`#${SECTIONS.CONTENT}`}
-      className="absolute left-1/2 top-0 z-50 -translate-x-1/2 -translate-y-full rounded-t-none transition-all focus-visible:translate-y-0"
+      className="absolute left-1/2 top-0 z-50 min-w-max -translate-x-1/2 -translate-y-full rounded-t-none opacity-0 transition-all focus-visible:translate-y-0 focus-visible:opacity-100"
     >
       Skip to Content
     </AnchorButton>
