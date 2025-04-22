c5de8a286e53f23f7540d273af4b68b444283704
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-22 16:49:10 -0700

    Remove redundant class names

--- src/components/common/buttons/hooks.ts
+++ src/components/common/buttons/hooks.ts
@@ -6,9 +6,9 @@ export const useButtonClassname = (size: ButtonSize) => {
   const sizeClassName = useMemo(() => {
     switch (size) {
       case "sm":
-        return "px-4 py-2 text-sm tracking-widest";
+        return "px-4 py-2 text-sm";
       case "md":
-        return "px-10 py-3 text-base tracking-widest";
+        return "px-10 py-3 text-base";
     }
   }, [size]);
 
@@ -17,7 +17,7 @@ export const useButtonClassname = (size: ButtonSize) => {
     // "border-accent text-accent font-inter rounded border font-medium tracking-wider transition-all duration-200 hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_#6da2ff]",
 
     // Style 2: pops more, better for grabbing attention
-    "text-primary font-inter rounded font-medium tracking-wider transition-all from-accent/35 to-highlight/65 bg-gradient-to-r hover:hue-rotate-[15deg] font-semibold active:brightness-90",
+    "text-primary font-inter rounded font-medium tracking-widest transition-all from-accent/35 to-highlight/65 bg-gradient-to-r hover:hue-rotate-[15deg] font-semibold active:brightness-90",
 
     sizeClassName,
   );
