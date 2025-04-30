68966b1e904268053b5295c146dbd942ce6af4f1
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-30 16:51:09 -0700

    Remove unused hosts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "64.media.tumblr.com", // TODO REMOVE
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
